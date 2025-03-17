83b7d322505d0931249059967ed210b54cd960b5
kukurik.pn <kukurik.pn@ucu.edu.ua>
2025-03-17 17:17:45 +0200

    Creating flappy bird simply game enviroment and adding the pictures to make game more beautiful in future

--- snake.py
+++ snake.py
@@ -1,185 +1,150 @@
 import pygame
-import time
+import sys
 import random
 
+# Основні параметри
+WIN_WIDTH = 400
+WIN_HEIGHT = 600
+FPS = 60
+
+# Колір
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 200, 0)
+RED = (200, 0, 0)
+
+# Ініціалізація
 pygame.init()
-
-# Кольори
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-purple = (138, 43, 226)
-
-# Розміри екрану
-dis_width = 800
-dis_height = 600
-
-# Створення вікна
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Змійка на Python: 2 гравці!')
-
+screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("Flappy Bird - Покращена фізика")
 clock = pygame.time.Clock()
-snake_block = 20
-snake_speed = 10
 
-# Шрифти
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
+# Параметри пташки
+GRAVITY = 0.5
+FLAP_STRENGTH = -10
+MAX_DROP_SPEED = 10
 
-def your_score(score1, score2):
-    value1 = score_font.render(f"Змійка 1 (Чорна): {score1}", True, yellow)
-    value2 = score_font.render(f"Змійка 2 (Фіолетова): {score2}", True, yellow)
-    dis.blit(value1, [10, 10])
-    dis.blit(value2, [10, 50])
 
-def draw_snake(color, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, color, [x[0], x[1], snake_block, snake_block])
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 34
+        self.height = 24
+        self.velocity = 0
+        self.color = GREEN  # Тестовий колір
 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+    def update(self):
+        # Прискорення вниз через гравітацію
+        self.velocity += GRAVITY
+        # Обмеження максимальної швидкості падіння
+        if self.velocity > MAX_DROP_SPEED:
+            self.velocity = MAX_DROP_SPEED
+        # Оновлення позиції
+        self.y += self.velocity
 
-def gameLoop():
-    game_over = False
-    game_close = False
+        # Межі
+        if self.y < 0:
+            self.y = 0
+            self.velocity = 0
+        if self.y + self.height > WIN_HEIGHT:
+            self.y = WIN_HEIGHT - self.height
+            self.velocity = 0
 
-    # Змійка 1 (управління WASD)
-    x1 = dis_width / 4
-    y1 = dis_height / 2
-    x1_change = 0
-    y1_change = 0
-    snake1_List = []
-    Length_of_snake1 = 1
+    def flap(self):
+        self.velocity = FLAP_STRENGTH
 
-    # Змійка 2 (управління стрілки)
-    x2 = dis_width * 3 / 4
-    y2 = dis_height / 2
-    x2_change = 0
-    y2_change = 0
-    snake2_List = []
-    Length_of_snake2 = 1
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
 
-    # Їжа
-    foodx = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
 
-    while not game_over:
+class Pipe:
+    def __init__(self, x):
+        self.x = x
+        self.width = 60
+        self.gap = 150
+        self.speed = 3
 
-        while game_close:
-            dis.fill(blue)
-            message("Кінець гри! Натисни 'C' для продовження або 'Q' для виходу", red)
-            your_score(Length_of_snake1 - 1, Length_of_snake2 - 1)
-            pygame.display.update()
+        self.top_height = random.randint(50, WIN_HEIGHT - self.gap - 50)
+        self.bottom_y = self.top_height + self.gap
 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
+    def update(self):
+        self.x -= self.speed
 
-        # Управління обох гравців
+    def draw(self, surface):
+        pygame.draw.rect(surface, RED, (self.x, 0, self.width, self.top_height))
+        pygame.draw.rect(surface, RED, (self.x, self.bottom_y, self.width, WIN_HEIGHT - self.bottom_y))
+
+    def collide(self, bird):
+        bird_rect = pygame.Rect(bird.x, bird.y, bird.width, bird.height)
+        top_pipe_rect = pygame.Rect(self.x, 0, self.width, self.top_height)
+        bottom_pipe_rect = pygame.Rect(self.x, self.bottom_y, self.width, WIN_HEIGHT - self.bottom_y)
+
+        return bird_rect.colliderect(top_pipe_rect) or bird_rect.colliderect(bottom_pipe_rect)
+
+
+def main():
+    bird = Bird(50, WIN_HEIGHT // 2)
+    pipes = []
+    score = 0
+    running = True
+    pipe_timer = 0
+    PIPE_SPAWN_INTERVAL = 1500  # мс
+
+    while running:
+        dt = clock.tick(FPS)
+        screen.fill(WHITE)
+
+        # --- Event handler ---
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_over = True
+                running = False
 
+            # Основне управління flap'ом
             if event.type == pygame.KEYDOWN:
-                # Змійка 1 - WASD
-                if event.key == pygame.K_a and x1_change == 0:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_d and x1_change == 0:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_w and y1_change == 0:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_s and y1_change == 0:
-                    y1_change = snake_block
-                    x1_change = 0
+                if event.key == pygame.K_SPACE:
+                    bird.flap()
 
-                # Змійка 2 - Стрілки
-                elif event.key == pygame.K_LEFT and x2_change == 0:
-                    x2_change = -snake_block
-                    y2_change = 0
-                elif event.key == pygame.K_RIGHT and x2_change == 0:
-                    x2_change = snake_block
-                    y2_change = 0
-                elif event.key == pygame.K_UP and y2_change == 0:
-                    y2_change = -snake_block
-                    x2_change = 0
-                elif event.key == pygame.K_DOWN and y2_change == 0:
-                    y2_change = snake_block
-                    x2_change = 0
+        # --- Оновлення об'єктів ---
+        bird.update()
 
-        # Оновлюємо позиції
-        x1 += x1_change
-        y1 += y1_change
-        x2 += x2_change
-        y2 += y2_change
+        # Створення нових труб
+        pipe_timer += dt
+        if pipe_timer > PIPE_SPAWN_INTERVAL:
+            pipes.append(Pipe(WIN_WIDTH))
+            pipe_timer = 0
 
-        # Перевірка виходу за межі
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
-        if x2 >= dis_width or x2 < 0 or y2 >= dis_height or y2 < 0:
-            game_close = True
+        # Оновлення труб і перевірка на зіткнення
+        for pipe in pipes:
+            pipe.update()
+            if pipe.collide(bird):
+                print(f"Game Over! Score: {score}")
+                running = False
 
-        dis.fill(blue)
-        # Малюємо їжу
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        # Видалення труб, що вийшли за межі
+        pipes = [pipe for pipe in pipes if pipe.x + pipe.width > 0]
 
-        # Оновлюємо списки змійок
-        snake1_Head = [x1, y1]
-        snake1_List.append(snake1_Head)
-        if len(snake1_List) > Length_of_snake1:
-            del snake1_List[0]
+        # --- Рендер ---
+        bird.draw(screen)
+        for pipe in pipes:
+            pipe.draw(screen)
 
-        snake2_Head = [x2, y2]
-        snake2_List.append(snake2_Head)
-        if len(snake2_List) > Length_of_snake2:
-            del snake2_List[0]
+        # Відображення рахунку
+        font = pygame.font.SysFont(None, 36)
+        score_surface = font.render(f"Score: {score}", True, BLACK)
+        screen.blit(score_surface, (10, 10))
 
-        # Перевірка зіткнень з власним тілом
-        for segment in snake1_List[:-1]:
-            if segment == snake1_Head:
-                game_close = True
-        for segment in snake2_List[:-1]:
-            if segment == snake2_Head:
-                game_close = True
+        pygame.display.flip()
 
-        # Перевірка зіткнень одна з одною
-        if snake1_Head in snake2_List:
-            game_close = True
-        if snake2_Head in snake1_List:
-            game_close = True
-
-        # Малюємо змійок
-        draw_snake(black, snake1_List)
-        draw_snake(purple, snake2_List)
-
-        your_score(Length_of_snake1 - 1, Length_of_snake2 - 1)
-
-        pygame.display.update()
-
-        # Перевірка чи хтось з'їв їжу
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
-            Length_of_snake1 += 1
-
-        if x2 == foodx and y2 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 20.0) * 20.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 20.0) * 20.0
-            Length_of_snake2 += 1
-
-        clock.tick(snake_speed)
+        # Підрахунок очок (коли пташка успішно пролетіла трубу)
+        for pipe in pipes:
+            if not hasattr(pipe, "passed") and pipe.x + pipe.width < bird.x:
+                pipe.passed = True
+                score += 1
 
     pygame.quit()
-    quit()
+    sys.exit()
 
-gameLoop()
+
+if __name__ == "__main__":
+    main()
